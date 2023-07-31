70856645d28f8e585725383f96f454fecdf70bd9
kiyshy <1073618744@qq.com>
2023-08-01 00:51:46 +0800

    tg job-sacn

---
+++ go.mod
@@ -0,0 +1,5 @@
+module job-search
+
+go 1.19
+
+require github.com/Arman92/go-tdlib v1.0.0 // indirect
---
+++ telegram/HandlerTelegram.go
@@ -0,0 +1,73 @@
+package telegram
+
+import (
+	"fmt"
+	"github.com/Arman92/go-tdlib"
+)
+
+func ListerTelegram() {
+
+	// 创建一个新的 TDLib 客户端
+	tdlib.SetLogVerbosityLevel(1)
+	tdlib.SetFilePath("./errors.txt")
+
+	// Create new instance of client
+	client := tdlib.NewClient(tdlib.Config{
+		APIID:               "187786",
+		APIHash:             "e782045df67ba48e441ccb105da8fc85",
+		SystemLanguageCode:  "en",
+		DeviceModel:         "Server",
+		SystemVersion:       "1.0.0",
+		ApplicationVersion:  "1.0.0",
+		UseMessageDatabase:  true,
+		UseFileDatabase:     true,
+		UseChatInfoDatabase: true,
+		UseTestDataCenter:   false,
+		DatabaseDirectory:   "./tdlib-db",
+		FileDirectory:       "./tdlib-files",
+		IgnoreFileNames:     false,
+	})
+
+	// 监听新消息
+	for {
+		// 连接到 Telegram 服务器并等待认证
+		currentState, _ := client.Authorize()
+		if currentState.GetAuthorizationStateEnum() == tdlib.AuthorizationStateWaitPhoneNumberType {
+			fmt.Print("Enter phone: ")
+			var number string
+			fmt.Scanln(&number)
+			_, err := client.SendPhoneNumber(number)
+			if err != nil {
+				fmt.Printf("Error sending phone number: %v", err)
+			}
+		} else if currentState.GetAuthorizationStateEnum() == tdlib.AuthorizationStateWaitCodeType {
+			fmt.Print("Enter code: ")
+			var code string
+			fmt.Scanln(&code)
+			_, err := client.SendAuthCode(code)
+			if err != nil {
+				fmt.Printf("Error sending auth code : %v", err)
+			}
+		} else if currentState.GetAuthorizationStateEnum() == tdlib.AuthorizationStateWaitPasswordType {
+			fmt.Print("Enter Password: ")
+			var password string
+			fmt.Scanln(&password)
+			_, err := client.SendAuthPassword(password)
+			if err != nil {
+				fmt.Printf("Error sending auth password: %v", err)
+			}
+		} else if currentState.GetAuthorizationStateEnum() == tdlib.AuthorizationStateReadyType {
+			fmt.Println("Authorization Ready! Let's rock")
+			break
+		}
+	}
+
+	// Main loop
+	rawUpdates := client.GetRawUpdatesChannel(100)
+	for update := range rawUpdates {
+		// Show all updates
+		fmt.Println(update.Data)
+		fmt.Print("\n\n")
+	}
+
+}
